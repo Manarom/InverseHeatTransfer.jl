2abaf0365a874f541fc8df686ef1dfc390299e20
Manarom <manarom@yandex.ru>
2026-02-12 18:17:09 +0300

    het

--- src/InverseHeatTransfer.jl
+++ src/InverseHeatTransfer.jl
@@ -1,6 +1,9 @@
 module InverseHeatTransfer
-
+using LinearAlgebra,Reexport
+export HeatTransferProblem
 # Write your package code here.
 include(joinpath(".","solvers", "OneDHeatTransfer.jl"))
+@reexport using .OneDHeatTransfer
 include(joinpath(".","polynomials", "PolynomialWrappers.jl"))
+@reexport using .PolynomialWrappers
 end
